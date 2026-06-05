e9edd7bdab97ae54c4079f0f30ed9ebceee5031d
bittu9975 <99003738+bittu9975@users.noreply.github.com>
2026-06-05 23:07:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+const form = document.getElementById("login-form");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const checkbox = document.getElementById("checkbox");
+const existingBtn = document.getElementById("existing");
+
+
+const savedUsername = localStorage.getItem("username");
+const savedPassword = localStorage.getItem("password");
+
+if (savedUsername && savedPassword) {
+  existingBtn.style.display = "block";
+}
+
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+
+  alert(`Logged in as ${username}`);
+
+  if (checkbox.checked) {
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+
+    existingBtn.style.display = "block";
+  } else {
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+
+    existingBtn.style.display = "none";
+  }
+});
+
+
+existingBtn.addEventListener("click", function () {
+  const username = localStorage.getItem("username");
+
+  if (username) {
+    alert(`Logged in as ${username}`);
+  }
+});
